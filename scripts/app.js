2bc74dbb95fac91e7d5195e2e52d9b4eeb0ad2cf
fluktar <tyc.pawel@gmail.com>
2023-11-21 16:11:10 +0100

    add edit btn plus function and logic

--- scripts/app.js
+++ scripts/app.js
@@ -5,21 +5,33 @@ const TodosApp = {
   methods: {
     saveTodo(event) {
       event.preventDefault();
-      const newTodo = {
-        text: this.enteredTodoText,
-        id: new Date().toISOString(),
-      };
-      this.todos.push(newTodo);
+      if (this.editedTodoId) {
+        const todoId = this.editedTodoId;
+        const todoIndex = this.todos.findIndex(function (todoItem) {
+          return todoItem.id === todoId;
+        });
+        const updatedTodoItem = {
+          id: this.todos[todoIndex].id,
+          text: this.enteredTodoText,
+        };
+        this.todos[todoIndex] = updatedTodoItem;
+      } else {
+        const newTodo = {
+          text: this.enteredTodoText,
+          id: new Date().toISOString(),
+        };
+        this.todos.push(newTodo);
+      }
 
       this.enteredTodoText = "";
     },
-  },
-  startEditTodo(todoId) {
-    this.editedTodoId = todoId;
-    const todo = this.todos.find(function (todoItem) {
-      return todoItem.id === todoId;
-    });
-    this.enteredTodoText = todo.text;
+    startEditTodo(todoId) {
+      this.editedTodoId = todoId;
+      const todo = this.todos.find(function (todoItem) {
+        return todoItem.id === todoId;
+      });
+      this.enteredTodoText = todo.text;
+    },
   },
 };
 Vue.createApp(TodosApp).mount("#todos-app");

e298e48eb432a5826ca7c8f44e8df05eb3bdf699
fluktar <tyc.pawel@gmail.com>
2023-11-21 16:32:18 +0100

    update app.js, add a conection to database ...

--- scripts/app.js
+++ scripts/app.js
@@ -3,7 +3,7 @@ const TodosApp = {
     return { todos: [], enteredTodoText: "New todo...", editedTodoId: null };
   },
   methods: {
-    saveTodo(event) {
+    async saveTodo(event) {
       event.preventDefault();
       if (this.editedTodoId) {
         const todoId = this.editedTodoId;
@@ -17,10 +17,40 @@ const TodosApp = {
         this.todos[todoIndex] = updatedTodoItem;
         this.editedTodoId = null;
       } else {
+     
+
+
+
+        let response;
+
+        try {
+          response = await fetch("http://localhost:3005/todos", {
+            method: "POST",
+            body: JSON.stringify({
+              text: this.enteredTodoText,
+            }),
+            headers: {
+              "Content-Type": "application/json",
+            },
+          });
+        } catch (error) {
+          alert("Something went wrong!");
+          return;
+        }
+      
+        if (!response.ok) {
+          alert("Something went wrong!");
+          return;
+        }
+      
+        const responseData = await response.json();
+        
         const newTodo = {
-          text: this.enteredTodoText,
-          id: new Date().toISOString(),
-        };
+            text: this.enteredTodoText,
+            id: todoId,
+          };
+
+
         this.todos.push(newTodo);
       }
 
